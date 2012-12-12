acc444067eb6e8d8c4e67be726d80ed895e52961
Jacopo Urbani <jacopo@cs.vu.nl>
2012-12-12 21:39:54 +0100

    new version

--- src/arch/actions/RemoveDuplicates.java
+++ src/arch/actions/RemoveDuplicates.java
@@ -2,6 +2,7 @@ package arch.actions;
 
 import arch.ActionContext;
 import arch.chains.Chain;
+import arch.data.types.DataProvider;
 import arch.data.types.Tuple;
 import arch.storage.container.WritableContainer;
 
@@ -9,42 +10,30 @@ public class RemoveDuplicates extends Action {
 
 	private final Tuple tuple = new Tuple();
 	boolean first = true;
-	long filtered = 0;
-	long total = 0;
 
 	@Override
 	public void startProcess(ActionContext context, Chain chain)
 			throws Exception {
 		first = true;
-		filtered = total = 0;
 	}
 
 	@Override
 	public void process(ActionContext context, Chain chain, Tuple inputTuple,
 			WritableContainer<Tuple> output,
 			WritableContainer<Chain> chainsToProcess) throws Exception {
-
-		if (inputTuple == null) {
-			if (!first) {
-				output.add(tuple);
+		if (first) {
+			inputTuple.copyTo(tuple);
+			output.add(inputTuple);
+			first = false;
+		} else if (inputTuple.compareTo(tuple) != 0) {
+			if (tuple.toString(new DataProvider()).equals(
+					inputTuple.toString(new DataProvider()))) {
+				System.out.println("Problem");
 			}
+			inputTuple.copyTo(tuple);
+			output.add(inputTuple);
 		} else {
-			if (first) {
-				inputTuple.copyTo(tuple);
-				first = false;
-			} else {
-				if (tuple.compareTo(inputTuple) != 0) {
-					output.add(tuple);
-					inputTuple.copyTo(tuple);
-				}
-			}
+			System.out.println("Found duplicates");
 		}
-	}
-
-	@Override
-	public void stopProcess(ActionContext context, Chain chain,
-			WritableContainer<Tuple> output,
-			WritableContainer<Chain> chainsToSend) throws Exception {
-		process(context, chain, null, output, null);
 	}
 }
