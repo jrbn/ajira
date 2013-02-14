d81d415a0c9c923de5487d6d4d0fbdccced6e53b
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2013-02-14 16:29:29 +0100

    Changed stream order

--- src/nl/vu/cs/ajira/buckets/Bucket.java
+++ src/nl/vu/cs/ajira/buckets/Bucket.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -320,9 +321,8 @@ public class Bucket {
 					File cacheFile = File.createTempFile("cache", "tmp");
 					cacheFile.deleteOnExit();
 
-					BufferedOutputStream fout = new BufferedOutputStream(
-							new SnappyOutputStream(new FileOutputStream(
-									cacheFile)), 65536);
+					OutputStream fout = new SnappyOutputStream(new BufferedOutputStream(new FileOutputStream(
+									cacheFile), 65536));
 
 					FDataOutput cacheOutputStream = new FDataOutput(fout);
 
@@ -341,9 +341,9 @@ public class Bucket {
 					cacheOutputStream.close();
 
 					// Register file in the list of cachedBuffers
-					FDataInput is = new FDataInput(new BufferedInputStream(
-							new SnappyInputStream(
-									new FileInputStream(cacheFile)), 65536));
+					FDataInput is = new FDataInput(new SnappyInputStream(
+									new BufferedInputStream(
+									new FileInputStream(cacheFile), 65536)));
 					synchronized (Bucket.this) {
 						if (!sort) {
 							cacheFiles.add(is);
